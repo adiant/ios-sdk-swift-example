e1f1b76fe2c0c5922da5b4db9c2fb3acad182db0
Joe Frazer <frazer@adiant.com>
2015-09-04 11:08:23 -0400

    add rich media example add slideshow example

--- ios-sdk-swift-example/InterstitialViewController.swift
+++ ios-sdk-swift-example/InterstitialViewController.swift
@@ -3,11 +3,14 @@ import AdbladeSDK
 
 // example interstitial view
 class InterstitialViewController: UIViewController, AdbladeDelegate {
+    internal var containerId: String = ""
+    internal var adType: AdbladeAdType = .mediumRectangle
+    
     override func loadView() {
         super.loadView()
         
         // make a new ad view and set it as this controller's view
-        let abView: AdbladeInterstitialView = AdbladeViewFactory.createView("13323-2709803565", viewType: .interstitial, delegate: self) as! AdbladeInterstitialView
+        let abView: AdbladeInterstitialView = AdbladeViewFactory.createView(self.containerId, adType: self.adType, delegate: self) as! AdbladeInterstitialView
         self.view = abView
     }
     
